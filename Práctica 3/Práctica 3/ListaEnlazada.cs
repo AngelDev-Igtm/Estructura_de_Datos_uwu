b36263e4b13a8220b30610c664ffe58c23754ea1
Freddy José Rosales Mogollón <n00448082@upn.pe>
2026-03-27 21:42:11 -0500

    Práctica_JP_Semana1
    Práctica de JP 27/03/2026

---
+++ Práctica 3/Práctica 3/ListaEnlazada.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_3
+{
+    public class ListaEnlazada
+    {
+        private Nodo primero;
+        public void Agregar (string nombre)
+        {
+            Nodo nuevoNodo = new Nodo { Nombre = nombre, Siguiente = null };
+            if (primero == null)
+            {
+                primero = nuevoNodo;
+            }
+            else
+            {
+                Nodo actual = primero;
+                while (actual.Siguiente != null)
+                {
+                    actual = actual.Siguiente;
+                }
+                actual.Siguiente = nuevoNodo;
+            }
+        }
+        //no pueden estar juntos los dos métodos Agregar, por eso se sobrecarga el método,
+        //uno recibe un string y el otro no recibe nada, pero ambos se llaman Agregar
+
+        public void Agregar()
+        {
+            Console.Write("Ingrese el nombre: ");
+            string nombre = Console.ReadLine();
+            Agregar(nombre);
+        }
+        public void Mostrar()
+        {
+            Nodo actual = primero;
+            while (actual != null)
+            {
+                Console.WriteLine(actual.Nombre);
+                actual = actual.Siguiente;
+            }
+        }
+        public bool Eliminar(string nombre)
+        {
+            Nodo temporal = primero;
+            Nodo anterior = null;
+            while (temporal != null)
+            { 
+                if (temporal.Nombre == nombre)
+                {
+                    if (temporal == primero)
+                    {
+                        // si el nodo a eliminar es el primero, actualizar la referencia "primero"
+                        primero = temporal.Siguiente;
+                    }
+                    else 
+                    {
+                        anterior.Siguiente = temporal.Siguiente;
+                    }
+                    return true;
+                }
+                anterior = temporal;
+                temporal = temporal .Siguiente;
+            }
+            return false;
+        }
+    }
+}
---
+++ Práctica 3/Práctica 3/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Práctica_3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion;
+            ListaEnlazada lista = new ListaEnlazada();
+            do
+            {
+                Console.WriteLine("  *___________________________* ");
+                Console.WriteLine("  |      Menú de Opciones     |");
+                Console.WriteLine("  |    1. Registrar Amigo     |");
+                Console.WriteLine("  |     2. Mostrar Lista      |");
+                Console.WriteLine("  |    3. Eliminar Amigo      |");
+                Console.WriteLine("  |        4. Salir           |");
+                Console.WriteLine("  *---------------------------*");
+                Console.WriteLine("  Seleccione: ");
+                string entrada = Console.ReadLine();
+                if (!int.TryParse(entrada, out opcion))
+                {
+                    Console.WriteLine("Entrada inválida. Intente de nuevo.");
+                    continue;
+                }
+
+                switch (opcion)
+                {
+                    case 1:
+                        lista.Agregar();
+                        break;
+                    case 2:
+                        lista.Mostrar();
+                        break;
+                    case 3:
+                        Console.Write("Ingrese el nombre del amigo a eliminar: ");
+                        string b = Console.ReadLine();
+                        if (lista.Eliminar(b))
+                            Console.WriteLine("Amigo eliminado.");
+                        else
+                            Console.WriteLine("Amigo no encontrado.");
+                        break;
+                    case 4:
+                        Console.WriteLine("Saliendo...");
+                        break;
+                    default:
+                        Console.WriteLine("Opción inválida.");
+                        break;
+                }
+            } while (opcion != 4);
+        
+        }
+    }
+}
